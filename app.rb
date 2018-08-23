4d026e40ec530c82900bb4cabf388ce6f16739f7
Albrecht Oster <a.oster@online.de>
2018-08-23 14:31:14 +0200

    add action to save a list of all addresses to a file

--- app.rb
+++ app.rb
@@ -4,7 +4,11 @@ require 'byebug'
 require './address_parser'
 require './letters_pdf'
 
-vcf_file = ARGV.first
+action = ARGV.first
+
+abort('Please specify one of the following actions: list, letters.') unless %w[letters list].include? action
+
+vcf_file = ARGV.last
 
 abort('Please specify a CSV file containing addresses.') if vcf_file.nil?
 abort('Specified CSV file does not exist.') unless File.exist? vcf_file
@@ -17,11 +21,21 @@ end
 
 addresses.sort_by! { |address| address[:plz] }
 
-pdf = FasTMassMailing::LettersPDF.new
+case action
+when 'letters'
+  pdf = FasTMassMailing::LettersPDF.new
 
-addresses.each_with_index do |address, i|
-  puts "Adding letter #{i+1} of #{addresses.count}"
-  pdf.add_letter(address)
+  addresses.each_with_index do |address, i|
+    puts "Adding letter #{i + 1} of #{addresses.count}"
+    pdf.add_letter(address)
+  end
+
+  pdf.render_file('letters.pdf')
+
+when 'list'
+  open('addresses.txt', 'w') do |f|
+    addresses.each do |address|
+      f.puts "#{address[:name]} – #{address[:street]} – #{address[:plz]} #{address[:city]}\n"
+    end
+  end
 end
-
-pdf.render_file('letters.pdf')
